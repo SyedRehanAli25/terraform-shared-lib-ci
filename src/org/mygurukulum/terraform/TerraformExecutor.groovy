9f2b29cfa007e6606928f9080e9ee7a1716ae8fe
Syed Rehan Ali <rehan.ali9325@gmail.com>
2026-01-23 02:05:53 +0530

    Executor

--- src/org/mygurukulum/terraform/TerraformExecutor.groovy
+++ src/org/mygurukulum/terraform/TerraformExecutor.groovy
@@ -1,1 +1,37 @@
-// Placeholder for TerraformExecutor.groovy
+package org.mygurukulum.terraform
+
+class TerraformExecutor implements Serializable {
+
+    def steps
+
+    TerraformExecutor(steps) {
+        this.steps = steps
+    }
+
+    def init(Map params = [:]) {
+        def path = params.get('path', '.')
+        steps.sh "cd ${path} && terraform init"
+    }
+
+    def validate(Map params = [:]) {
+        def path = params.get('path', '.')
+        steps.sh "cd ${path} && terraform validate"
+    }
+
+    def plan(Map params = [:]) {
+        def path = params.get('path', '.')
+        def outFile = params.get('out', 'plan.tfplan')
+        steps.sh "cd ${path} && terraform plan -out=${outFile}"
+    }
+
+    def apply(Map params = [:]) {
+        def path = params.get('path', '.')
+        def planFile = params.get('plan', 'plan.tfplan')
+        steps.sh "cd ${path} && terraform apply ${planFile}"
+    }
+
+    def destroy(Map params = [:]) {
+        def path = params.get('path', '.')
+        steps.sh "cd ${path} && terraform destroy -auto-approve"
+    }
+}
